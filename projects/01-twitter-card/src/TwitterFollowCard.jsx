1a03f534e1054388506d46088d709f0ef898d2be
Lluis Alfonso <lluisalfonso10@gmail.com>
2023-11-15 21:58:51 +0100

    [01-twitter-card] -  Twitter card fixed first project

--- projects/01-twitter-card/src/TwitterFollowCard.jsx
+++ projects/01-twitter-card/src/TwitterFollowCard.jsx
@@ -25,7 +25,8 @@ export function TwitterFollowCard({ children, formatUserName, userName = 'unknow
 
             <aside>
                 <button className={buttonClassName} onClick={handleClick}>
-                    {text}
+                    <span className="tw-followCard-text">{text}</span>
+                    <span className="tw-followCard-stopFollow">Dejar de seguir</span>
                 </button>
             </aside>
         </article>
